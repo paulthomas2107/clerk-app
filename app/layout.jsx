4b981938830a9bc9a32ab4a546e41c750ce1d787
Paul Thomas <paulthomas2107@gmail.com>
2023-07-17 12:48:10 +0100

    more

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,7 +15,8 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <main className="container mx-auto">
+          <Header></Header>
+          <main className="container">
             <div className="flex items-start justify-center min-h-screen">
               <div className="mt-20">{children}</div>
             </div>
